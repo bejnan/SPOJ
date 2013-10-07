f6c3a40c45ae52a269ee642d4c2ada480dcd39da
Jakub Banaszewski <bananow@gmail.com>
2013-10-07 15:43:46 +0200

    Zaakceptowane zadanie

--- Solutions/JSPACE.cpp
+++ Solutions/JSPACE.cpp
@@ -2,8 +2,26 @@
 
 using namespace std;
 
+const int TO_UPPER = 'a' - 'A';
 
-int main()
-{
+int main() {
+	istreambuf_iterator<char> eos;
+	string input(istreambuf_iterator<char>(cin), eos);
+	string output;
+	bool makeBig = false;
+	char replacement;
+	for (int i = 0; i < input.length(); i++) {
+		replacement = input[i];
+		if (input[i] == ' ') {
+			makeBig = true;
+		} else {
+			if (makeBig && input[i] > 96 && input[i] < 123)
+				replacement -= TO_UPPER;
+			output += replacement;
+			makeBig = false;
+		}
+
+	}
+	cout << output;
 	return 0;
 }
