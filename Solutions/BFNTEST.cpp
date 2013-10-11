7a1210b98477c5f65903b16861c4d2cf7e5765d5
Jakub Banaszewski <bananow@gmail.com>
2013-10-11 13:46:43 +0200

    Good solution

--- Solutions/BFNTEST.cpp
+++ Solutions/BFNTEST.cpp
@@ -4,74 +4,60 @@
 #include<iostream>
 
 using namespace std;
-enum state{
-	normal = 0, short_comment, long_comment, text
+enum state {
+	normal = 0, maybe_comment, short_comment, long_comment, maybe_uncomment
 };
 
 int main_BFNTEST() {
 	istreambuf_iterator<char> eos;
 	string input(istreambuf_iterator<char>(cin), eos);
 	string output;
-	state read_state;
+	state read_state = normal;
 	int length = input.length();
 	char replacement;
-
-	for (int i = 0; i < length; i++)
-	{
+	char last_replacement;
+	for (int i = 0; i < length; i++) {
 		replacement = input[i];
-		if (read_state == normal && input[i] == '"')
-		{
-			read_state = text;
-			output += replacement;
-		}
-		else if (read_state == text && input[i] == '"')
-		{
-			read_state = normal;
-			output += replacement;
-		}
-		else if (input[i] == '/' && read_state==normal)
-		{
-			if (i < length - 1 )
-			{
-				if (input[i+1] == '/')
-				{
-					read_state = short_comment;
-					i++;
-				}
-				else if (input[i+1] == '*')
-				{
-					read_state = long_comment;
-					i++;
-				}
-				else
-				{
-					output += replacement;
-				}
-			}
-			else
-			{
+		switch (read_state) {
+
+		case normal:
+			if (replacement == '/') {
+				read_state = maybe_comment;
+				last_replacement = replacement;
+			} else
 				output += replacement;
+			break;
+
+		case maybe_comment:
+			if (replacement == '/')
+				read_state = short_comment;
+			else if (replacement == '*')
+				read_state = long_comment;
+			else {
+				read_state = normal;
+				output = output + last_replacement + replacement;
 			}
-		}
-		else if (read_state == short_comment && input[i]== '\n')
-		{
-			read_state = normal;
-			output += replacement;
-		}
-		else if (read_state == long_comment && input[i] == '*')
-		{
-			if (i < length - 1 )
-			{
-				if (input[i+1] == '/')
-				{
-					read_state = normal;
-					i++;
-				}
+			break;
+
+		case short_comment:
+			if (replacement == '\n') {
+				read_state = normal;
+				output = output + replacement;
 			}
-		}
-		else if (read_state == normal || read_state == text)
-		{
+			break;
+		case long_comment:
+			if (replacement == '*')
+				read_state = maybe_uncomment;
+			break;
+		case maybe_uncomment:
+			if (replacement == '/')
+				read_state = normal;
+			else
+				read_state = long_comment;
+			break;
+		default:
 			output += replacement;
+			break;
 		}
 	}
 	cout << output;
